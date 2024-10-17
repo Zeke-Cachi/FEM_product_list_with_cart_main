8fae1e0cf99f86cd156c7c739f08f1fdaa9c2d88
Cachi <ezequielcachi@gmail.com>
2024-10-17 17:14:29 -0300

    styled cart cards

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -11,19 +11,31 @@ const CartCard: React.FC<{
   };
 
   return (
-    <div>
-      <h3>{item.name}</h3>
-      <div>
-        <div>
-          <p>{item.quantity}x</p>
-          <p>{item.price}</p>
-          <p>{item.price * item.quantity!}</p>
+    <>
+      <div className="flex justify-between items-center px-4">
+        <div className="flex flex-col gap-2">
+          <h3 className="font-semibold text-project_rose_900">{item.name}</h3>
+          <div className="flex gap-2">
+            <p className="text-project_red me-2 font-semibold">
+              {item.quantity}x
+            </p>
+            <p className="text-project_rose_400">@${item.price.toFixed(2)}</p>
+            <p className="text-project_rose_500 font-semibold">
+              ${(item.price * item.quantity!).toFixed(2)}
+            </p>
+          </div>
         </div>
-        <button aria-label="button" onClick={() => removeProductFromCart(item)}>
-          <img src={removeIcon} alt="image of a remove icon" />
-        </button>
+        <div className="grid place-items-center rounded-full h-4 w-4 border-solid border border-project_rose_500">
+          <button
+            aria-label="button"
+            onClick={() => removeProductFromCart(item)}
+          >
+            <img src={removeIcon} alt="image of a remove icon" />
+          </button>
+        </div>
       </div>
-    </div>
+      <hr className="text-purple-500 w-5/6 h-4 block mx-auto mt-4" />
+    </>
   );
 };
 
