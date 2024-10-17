1f91f342c8b19307f00fa6f7891d407421ebe511
Cachi <ezequielcachi@gmail.com>
2024-10-17 17:35:06 -0300

    finished styling cart component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -2,6 +2,7 @@ import { productDataType } from "../types";
 import { Dispatch, SetStateAction } from "react";
 import CartCard from "./CartCard";
 import emptyCartImg from "../../public/assets/images/illustration-empty-cart.svg";
+import carbonNeutralImg from "../../public/assets/images/icon-carbon-neutral.svg";
 
 const Cart: React.FC<{
   cartItems: productDataType[];
@@ -9,7 +10,7 @@ const Cart: React.FC<{
 }> = ({ cartItems, setCartItems }) => {
   return (
     <div>
-      <h2 className="text-2xl font-bold lg:mt-8 text-project_red">
+      <h2 className="text-2xl font-bold lg:mt-8 text-project_red lg:mb-8">
         Your Cart (
         {cartItems.reduce((counter, item) => counter + (item.quantity || 0), 0)}
         )
@@ -31,6 +32,28 @@ const Cart: React.FC<{
           ))}
         </div>
       )}
+      <div className="flex justify-between px-2 lg:mt-8">
+        <p className="text-project_rose_900">Order total</p>
+        <h4 className="text-2xl font-bold">
+          $
+          {cartItems
+            .reduce((acc, item) => acc + item.price * item.quantity!, 0)
+            .toFixed(2)}
+        </h4>
+      </div>
+      <div className="flex gap-2 my-8 justify-center text-project_rose_900">
+        <img src={carbonNeutralImg} alt="carbon neutral icon" />
+        <p>
+          This is a <span className="font-semibold">carbon neutral</span>{" "}
+          delivery
+        </p>
+      </div>
+      <button
+        className="h-14 w-full mx-auto bg-project_red 
+              rounded-full font-semibold flex justify-center items-center gap-2 border-solid border-project_rose_900 text-white"
+      >
+        Confirm order
+      </button>
     </div>
   );
 };
